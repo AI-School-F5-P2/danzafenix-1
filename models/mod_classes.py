cc7eb051968f0073b234d6326e0627a2a40ef264
Ana Gomez <anitaggrr@gmail.com>
2023-07-23 09:40:09 +0200

    modelprices

--- models/mod_classes.py
+++ models/mod_classes.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from config.database import Base
+from models.mod_prices import ModelPacks
 
 
 class ModelClasses(Base):
@@ -25,7 +26,11 @@ class ClassesLevels(Base):
 
     __tablename__ = "classes-levels"
 
-    id_cla1 = Column(Integer, ForeignKey("classes.id_cla"), primary_key = True)
-    id_level1 = Column(Integer, ForeignKey("Levels.id_level"), primary_key = True)
+    id_cla_level = Column(Integer, primary_key = True, index = True)
+    id_cla1 = Column(Integer, ForeignKey("classes.id_cla"))
+    id_level1 = Column(Integer, ForeignKey("Levels.id_level"))
     classes1 = relationship("ModelClasses", back_populates = "levels1")
     levels1 = relationship("ModelLevels", back_populates = "classes1")
+    id_pac1 = Column(Integer, ForeignKey("packs.id_pac"))
+    packs1 = relationship("ModelPacks", back_populates = "class_level")
+
--- models/mod_prices.py
+++ models/mod_prices.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy.orm import relationship, column_property
 from config.database import Base
 
 class ModelPacks(Base):
@@ -8,3 +8,17 @@ class ModelPacks(Base):
 
     id_pac = Column(Integer, primary_key = True, index = True)
     name_pac = Column(String(50), nullable = False)
+    class_level = relationship("ClassesLevels", back_populates = "packs1")
+
+
+class ModelPrices(Base):
+
+    __tablename__ = "prices-discounts"
+
+    id_pd = Column(Integer, primary_key = True, index = True)
+    type_pd = Column(String(50))
+    individual_price = Column(Float, nullable = False, default = 35.00)
+    discount2 = Column(Float, nullable = False, default = 0)
+    price2 = column_property((individual_price*(100 - discount2))/100)
+    discount3 = Column(Float, nullable = False, default = 0)
+    price3 = column_property((individual_price*(100 - discount3))/100)

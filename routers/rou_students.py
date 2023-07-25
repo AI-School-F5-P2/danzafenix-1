e90f174cebb769abebe08fa455a0f2c845db470c
Ana Gomez <anitaggrr@gmail.com>
2023-07-25 22:46:44 +0200

    inscripcion segregada por meses

--- routers/rou_students.py
+++ routers/rou_students.py
@@ -2,10 +2,13 @@ from fastapi import APIRouter, Path
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from typing import List
-from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_406_NOT_ACCEPTABLE
 from schemas.sch_students import StudentSchema, StudentClassSchema
+from schemas.sch_classes import RegistrationSchema
 from config.database import Session
 from models.mod_students import ModelStudents, StudentsClasses
+from models.mod_classes import ModelClasses, ModelLevels, ClassesLevels
+from sqlalchemy import extract
 
 
 student = APIRouter(prefix = "/api/student", tags = ["Students"])
@@ -46,13 +49,15 @@ def get_student_by_DNI(DNI_stu: str = Path(pattern = r'^([XYZ]\d{7}[A-Z]|\d{8}[A
 #pasamos como parámetro el esquema Student para hacer la validación de tipos
 #new_student = ModelStudents(**data_student.model_dump()). Los **indica que tomamos todos los datos como parámetros
 @student.post("/create", status_code = HTTP_201_CREATED)
-def create_student(data_student:StudentSchema):
+def create_student(data_student: StudentSchema):
     db = Session()
-    new_student = ModelStudents(**data_student.model_dump())
-    #hacer control en caso de DNI repetido
-    db.add(new_student)
-    db.commit()
-    return JSONResponse(status_code = HTTP_201_CREATED, content = {"message": "El registro estudiante se ha creado correctamente"})
+    verification = db.query(ModelStudents).filter(ModelStudents.DNI_stu == data_student.DNI_stu).first()
+    if not verification:
+        new_student = ModelStudents(**data_student.model_dump())
+        db.add(new_student)
+        db.commit()
+        return JSONResponse(status_code = HTTP_201_CREATED, content = {"message": "El registro estudiante se ha creado correctamente"})
+    return JSONResponse(status_code = HTTP_406_NOT_ACCEPTABLE, content = {"message": "El DNI ya se ha registrado previamente"})
 
 
 @student.put("/{id_stu}", response_model = StudentSchema)
@@ -115,10 +120,10 @@ def create_student_class(data_student_class: StudentClassSchema):
     return JSONResponse(status_code = HTTP_201_CREATED, content = {"message": "El registro estudiante y su respectiva clase se ha creado correctamente"})
 
 
-@student_class.put("/{id_stu1}", response_model = StudentSchema)
-def update_student(data_update: StudentClassSchema, id_stu1: int = Path(ge = 1)):
+@student_class.put("/{id_stu1}/{id_cla_level1}", response_model = StudentSchema)
+def update_student(data_update: StudentClassSchema, id_stu1: int = Path(ge = 1), id_cla_level1: int = Path(ge = 1)):
     db = Session()
-    result = db.query(StudentsClasses).filter(StudentsClasses.id_stu1 == id_stu1).first()
+    result = db.query(StudentsClasses).filter(StudentsClasses.id_stu1 == id_stu1, StudentsClasses.id_cla_level1 == id_cla_level1).first()
     if not result:
         return JSONResponse(status_code = HTTP_404_NOT_FOUND, content = {"message": "El ID no se ha encontrado en la base de datos"})
     result.id_stu1 = data_update.id_stu1
@@ -129,12 +134,32 @@ def update_student(data_update: StudentClassSchema, id_stu1: int = Path(ge = 1))
     return JSONResponse(status_code = HTTP_200_OK, content = {"message": "Los datos del estudiante y su respectiva clase se han modificado correctamente"})
 
 
-@student_class.delete("/{id_stu1}", status_code = HTTP_200_OK)
-def delete_student_class(id_stu1: int = Path(ge = 1)):
+@student_class.delete("/{id_stu1}/{id_cla_level1}", status_code = HTTP_200_OK)
+def delete_student_class(id_stu1: int = Path(ge = 1), id_cla_level1: int = Path(ge = 1)):
     db = Session()
-    result = db.query(StudentsClasses).filter(StudentsClasses.id_stu1 == id_stu1).first()
+    result = db.query(StudentsClasses).filter(StudentsClasses.id_stu1 == id_stu1, StudentsClasses.id_cla_level1 == id_cla_level1).first()
     if not result:
         return JSONResponse(status_code = HTTP_404_NOT_FOUND, content = {"message": "El ID no se ha encontrado en la base de datos"})
     db.delete(result)
     db.commit()
     return JSONResponse(status_code = HTTP_200_OK, content = {"message": "Se ha eliminado el registro correctamente"})
+
+
+@student_class.get("/by_month/{year}/{month}/", response_model = List[RegistrationSchema], status_code = HTTP_200_OK)
+def students_classes_by_month(year: int, month: int = Path(ge = 1, le = 12)):
+    db = Session()
+    result = db.query(ModelStudents.name_stu, ModelStudents.DNI_stu, ModelClasses.name_cla, ModelLevels.name_level
+                      ).join(StudentsClasses, ModelStudents.id_stu == StudentsClasses.id_stu1
+                             ).join(ClassesLevels, StudentsClasses.id_cla_level1 == ClassesLevels.id_cla_level
+                                    ).join(ModelClasses, ClassesLevels.id_cla1 == ModelClasses.id_cla
+                                           ).join(ModelLevels, ClassesLevels.id_level1 == ModelLevels.id_level
+                                                  ).filter(extract("month", StudentsClasses.registration_date) == month, 
+                                                          extract("year", StudentsClasses.registration_date) == year).all()
+    response_data = []
+    for name_stu, DNI_stu, name_cla, name_level in result:
+        response_data.append({
+            "name_stu": name_stu,
+            "DNI_stu": DNI_stu,
+            "name_cla": name_cla,
+            "name_level": name_level})
+    return JSONResponse(status_code = HTTP_200_OK, content = jsonable_encoder(response_data))
--- schemas/sch_classes.py
+++ schemas/sch_classes.py
@@ -13,3 +13,10 @@ class ClassLevelSchema(BaseModel):
     id_cla1: int = Field(ge = 1)
     id_level1: int = Field(ge = 1)
     id_pac1: int = Field(ge = 1)
+
+
+class RegistrationSchema(BaseModel):
+    name_stu: str
+    DNI_stu: str = Field(pattern = r'^([XYZ]\d{7}[A-Z]|\d{8}[A-HJ-NP-TV-Z])$') 
+    name_cla: str
+    name_level: str
